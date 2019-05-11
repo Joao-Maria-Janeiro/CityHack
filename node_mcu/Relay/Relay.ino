1d526ed34b649ad496fcef8fc30bbc3c41185b6f
Umelhor99 <39059647+Umelhor99@users.noreply.github.com>
2019-05-11 18:52:06 +0100

    relay folder

---
+++ node_mcu/Relay/Relay.ino
@@ -0,0 +1,31 @@
+#include <math.h>
+
+int pinOut = 10;
+int pinIn = 9;
+static bool circuit_on = false;
+
+void circuit_on_off() {
+  
+  if(circuit_on){
+    digitalWrite(pinOut, LOW);
+    circuit_on = false;
+  }else{
+    digitalWrite(pinOut, HIGH);
+    circuit_on = true;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinOut, OUTPUT);
+  pinMode(pinIn, INPUT);
+  
+  
+}
+
+void loop() {                   
+    if(digitalRead(pinIn))  circuit_on_off();
+      
+}
+
+
