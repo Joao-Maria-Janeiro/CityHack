de3a60830a982356180a3a83940ec281f2b5b765
João Maria Janeiro <joaomariajaneiro1@gmail.com>
2019-05-12 01:23:22 +0100

    Major changes

--- Back-End/nodes/models.py
+++ Back-End/nodes/models.py
@@ -5,9 +5,6 @@ class Day(models.Model):
     day_number = models.IntegerField(default=-1)
     energy_per_day = models.FloatField(default=-1.0)
 
-class DivionUser(models.Model):
-    name = models.CharField(max_length=100)
-    monthly_waste = models.FloatField(default=-1.0)
 
 class Plug(models.Model):
     activation_key = models.CharField(max_length=100)
@@ -19,7 +16,6 @@ class Plug(models.Model):
     days = models.ManyToManyField(Day, blank=True)
 
 class Division(models.Model):
-    users = models.ManyToManyField(DivionUser, blank=True)
     name = models.CharField(max_length=100)
     products = models.ManyToManyField(Plug, blank=True)
     daily_waste = models.FloatField(default=-1.0)
