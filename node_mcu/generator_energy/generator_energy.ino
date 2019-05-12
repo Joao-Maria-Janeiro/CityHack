65d2af36da9843cb9fd5953c0c0f8f90b2770255
Umelhor99 <39059647+Umelhor99@users.noreply.github.com>
2019-05-12 10:18:53 +0100

    medidor de tensão

--- node_mcu/generator_energy/generator_energy.ino
+++ node_mcu/generator_energy/generator_energy.ino
@@ -2,33 +2,24 @@
 #define equivalent_resistor 12.5
 
 void setup() {
-  pinMode(D0,LOW);
-  pinMode(D1,LOW);
   Serial.begin(9600);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, INPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
 
-  //int sensorValue = analogRead(A0)-analogRead(A1);
+  int sensorValue = analogRead(A2);
+  //Serial.println(sensorValue);
+  //analogWrite(A0, sensorValue);
 
-  
-  pinMode(D1,LOW);
-  pinMode(D0, HIGH);
-  float val1 = analogRead(A0);
-  pinMode(D0,LOW);
-  pinMode(D1, HIGH);
-  float val2 = analogRead(A0);
-  float sensorValue = val1-val2;
   // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float voltage = sensorValue * (5.0 / 1023.0);
-  float current = voltage/equivalent_resistor;
-  // print out the value you read:
-   //Serial.print(current*1000);
-   //Serial.println("mA");
-  float power = voltage_generator * (current);
-  Serial.print(power*1000);
-  Serial.println("mW");
-  delay(1000);
-
+  //Serial.print("voltage: ");
+  //Serial.println(voltage);
+  int voltage2 = 255 * voltage / 5;
+  Serial.println(voltage2);
+  Serial.write(voltage2);
+  
 }
